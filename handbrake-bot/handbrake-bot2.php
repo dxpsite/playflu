b298453fc48e22f0f9e31fb98983cdf14d1662e5
Сердюков Д.М <C:\Users\dserdukov\AppData\Roaming\The Bat!>
2019-03-14 10:34:59 +0300

    Rebuild all structure

--- handbrake-bot/handbrake-bot2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	
-		shell_exec("HandBrakeCLI -i /home/sample/files/1.avi -o /home/sample/files/1.mp4 -m -E copy –audio-copy-mask ac3,dts,dtshd –audio-fallback ffac3 -e x264 -q 20 -x level=4.1:ref=4:b-adapt=2:direct=auto:me=umh:subq=8:rc-lookahead=50:psy-rd=1.0,0.15:deblock=-1,-1:vbv-bufsize=30000:vbv-maxrate=40000:slices=4");
-		echo "\nEncoding \"".$new_media_file_name."\" Complete.";	
-
-	
-?>
--- handbrake-bot/playlist-bot.php
+++ handbrake-bot/playlist-bot.php
@@ -2,14 +2,11 @@
 echo "success started..";
     $db_host="localhost";
 	$db_user="root";
-	$db_password="trolimoli1218!";
+	$db_password="***************";
 	$db_name="dbase";
-	$playlists_dir="/home/playtube/crud/playlists/"; // Mediadrop Media directory, note the trailing slash
-	//$handbrake_bot_dir="/opt/handbrake-bot/";
+	$playlists_dir="/home/playflu/playlists/"; // directory, note the trailing slash
 	$sleep_duration= 600;				// Bot sleep duration in secs between checks
-	//$datetime = date_create()->format('Y-m-d H:i:s');
-	//$datetime = date_create()->format('Y-m-d H:i:s');
-//$datetime = date_create()->format('Y-m-d H:i:s');
+	
 
 	function connect_to_database($db_host, $db_user, $db_password) {
         $link = mysqli_connect($db_host, $db_user, $db_password);
@@ -24,9 +21,6 @@ echo "success started..";
 function get_playlist_from_database($link, $db_name) {
 	
 	mysqli_select_db($link,$db_name);
-	//mysqli_query ('SET NAMES utf8');
-	//$datetime = date_create()->format('Y-m-d H:i:s');
-	//$select_query = "SELECT * FROM playlists WHERE DATE(start) = CURDATE()";
 	$select_query = "SELECT * FROM playlists WHERE ( NOW() BETWEEN start AND end)";
 	//$select_query = "SELECT * FROM playlists WHERE start >= '2019-01-13 00:00:00' AND end <= '2019-01-19 00:00:00'";
 	$unencoded_media = mysqli_query($link, $select_query) or die(mysqli_error());
@@ -50,46 +44,18 @@ while(true){
 	$unencoded_media = get_playlist_from_database($link, $db_name);
 
 	while($row = mysqli_fetch_array($unencoded_media)){
-	//	foreach($row as $cname => $cvalue){
-     //   		print "$cname: $cvalue\t";
-	  //  	}
-
-		// Get File to be transcoded
 	    $playlist_name = $row['title'];
 	    $playlist_id = $row['id'];
-	    // $author_email = $row['author_email'];
-		//$new_media_file_name = replace_extension($media_file_name, $new_extension);
-		//$command = getTranscodeCommandString($handbrake_command, $handbrake_preset, $new_extension, $playlists_dir, $media_file_name, $new_media_file_name);
-
-	    	// transcode file
-	    	// TODO: support for multiple profiles
-		//shell_exec($command);
-		//echo "\nEncoding \"".$new_media_file_name."\" Complete.";	
-
-		//email_submitter($media_file_name);
-
-	//insert_new_movie_to_database($db_name, $playlists_dir, $new_media_file_name, $media_id);    
-
-		//mark_new_media_as_encoded($db_name, $new_media_file_name, $playlists_dir, $media_id, $media_size);
-
-		//generate_thumbnail_file($mediadrop_thumbnail_dir, $thumbnail_extension, $playlists_dir, $thumbnail_script_name, $row['unique_id'], $media_id);
-
 		echo $playlist_name."\n";
 		echo "File: playlist_".$playlist_id.".txt\n";
-
-		$file = "/home/playtube/crud/playlists/playlist_".$playlist_id.".txt";
-        $newfile = "/home/playtube/crud/shed/calendar/playlist_daily/playlist.txt";
-
+		$file = "/home/playflu/playlists/playlist_".$playlist_id.".txt";
+        $newfile = "/home/playflu/shed/calendar/playlist_daily/playlist.txt";
         if (!copy($file, $newfile)) {
         echo "Не удалось скопировать $file...\n";
 }
 
-		//email_submitter($playlist_name);
-
-
 
 	}
-//}
 	
 	mysqli_close($link);
 
